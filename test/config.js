2fbf58d34feabc9b8ff8030c3f4625d151e0fc9a
xicilion <lion@9465.net>
2025-06-03 20:25:19 +0800

    refactor: Replace custom task parsing logic with utility function in BaseDBAdapter and MemoryAdapter; streamline task processing

--- test/config.js
+++ test/config.js
@@ -9,7 +9,7 @@ if (process.argv.includes('--memory')) {
     module.exports = {
         // "dbConnection": "mysql://root:******@127.0.0.1:3306/test"
         // "dbConnection": "psql://postgres:******@127.0.0.1/test"
-        "dbConnection": "sqlite:test.db"
-        // "dbConnection": "sqlite::memory:"
+        // "dbConnection": "sqlite:test.db"
+        "dbConnection": "sqlite::memory:"
     };
 }
